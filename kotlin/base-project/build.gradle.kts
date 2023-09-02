3928fdbf4be287d5219e143b8a94622433b8cda4
MRoose <miramix00@gmail.com>
2023-09-03 02:28:07 +0300

    chp1

--- kotlin/base-project/build.gradle.kts
+++ kotlin/base-project/build.gradle.kts
@@ -13,7 +13,7 @@ kotlin {
 
 val kotlinVersion = "1.9.10"
 val slf4jApiVersion = "2.0.7"
-val lombokVersion = "1.18.28"
+val mapstructVersion = "1.5.5.Final"
 val jakartaEeBomVersion = "10.0.0"
 val apacheCommonsLangVersion = "3.13.0"
 val apacheCommonsIoVersion = "2.13.0"
@@ -25,13 +25,14 @@ val okHttpBomVersion = "4.11.0"
 val junitBomVersion = "5.10.0"
 val mockitoBomVersion = "5.5.0"
 val assertjBomVersion = "3.24.2"
+val testcontainersBomVersion = "1.19.0"
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:$kotlinVersion"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("org.slf4j:slf4j-api:$slf4jApiVersion")
-    compileOnly("org.projectlombok:lombok:$lombokVersion")
-    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
     implementation(platform("jakarta.platform:jakarta.jakartaee-bom:$jakartaEeBomVersion"))
     implementation("jakarta.annotation:jakarta.annotation-api")
     implementation("org.apache.commons:commons-lang3:$apacheCommonsLangVersion")
@@ -40,7 +41,7 @@ dependencies {
     implementation("com.google.guava:guava")
     implementation("com.google.code.gson:gson")
     implementation(platform("com.fasterxml.jackson:jackson-bom:$jacksonBomVersion"))
-    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.jayway.jsonpath:json-path:$jsonPathVersion")
     implementation(platform("com.squareup.okhttp3:okhttp-bom:$okHttpBomVersion"))
     implementation("com.squareup.okhttp3:okhttp")
@@ -51,6 +52,9 @@ dependencies {
     testImplementation("org.mockito:mockito-core")
     testImplementation(platform("org.assertj:assertj-bom:$assertjBomVersion"))
     testImplementation("org.assertj:assertj-core")
+    testImplementation(platform("org.testcontainers:testcontainers-bom:$testcontainersBomVersion"))
+    testImplementation("org.testcontainers:testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
 }
 
 tasks.withType<KotlinCompile> {
